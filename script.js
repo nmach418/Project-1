abc62dd933335095bf28185a01080e86f65f256a
Boku <Boku@NHs-MacBook-Pro.local>
2018-07-11 19:55:27 -0400

    Still no luck with JS logic. Add #ids to HTML and CSS files

--- script.js
+++ script.js
@@ -1,6 +1,54 @@
-console.log("The Dude Abides")
+console.log("Yosh!")
 
-const usStates = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+const usStates = ["What is the capital of Alabama?", "What is the capital of Alaska?", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
 const stateCaps = ["Montgomery", "Juneau", "Phoenix", "Little Rock", "Sacramento", "Denver", "Hartford", " Dover", "Tallahassee", "Atlanta", "Honolulu", "Boise", "Springfield", "Indianapolis", "Des Moines", "Topeka", "Frankfort", "Baton Rouge", "Augusta", "Annapolis", "Boston", "Lansing", "Saint Paul", "Jackson", "Jefferson City", "Helena", "Lincoln", "Carson City", "Concord", "Trenton", "Santa Fe", "Albany", "Raleigh", "Bismarck", "Columbus", "Oklahoma City", "Salem", "Harrisburg", "Providence", "Columbia", "Pierre", "Nashville", "Austin", "Salt Lake City", "Montpelier", "Richmond", "Olympia", "Charleston", "Madison", "Cheyenne"]
 
 
+
+
+for (let i = 0; i <= usStates.length; i++) {
+    document.querySelector("#city").innerHTML = usStates // This would not do the job even it worked.
+                                                         // It would interate through th entie array without stopping.    
+ 
+}
+
+
+
+
+// if (ansQ1 === "Montgomery") {
+// }
+
+
+
+/*
+// Note: ansQ1 below is textbox input - via DOM path...? Figure out how to do it.
+
+// if (ansQ1 == stateCaps[0]) {
+
+}
+
+
+// Think about how to get state names from usStates array into the question paragraph e.g.,: document.querySelector("#city").innerHTML = usStates (for looped?)
+// Write the questions directly into the usStates array???
+
+
+for (let i = 0; i <= usStates.length; i++) {    
+    if (usStates[i] === stateCaps) {
+        console.log("yes");
+    }
+    else {
+        console.log("no");
+    }
+ }
+
+
+
+// May not need to pass testbox input into a function as a variable. It may be better
+// to use the DOM to retrieve the data for your/the function
+
+ function stateCapCompare(textboxInput) {
+    if ()
+
+}
+
+*/
