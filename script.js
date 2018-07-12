172e0bb3dfedc90215423a7128f7ba410c089056
Boku <Boku@NHs-MacBook-Pro.local>
2018-07-12 07:36:18 -0400

    updated JS. Attempted to get  quiz questions from usStates array using querySelector and getElementId. No luck so far.

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 console.log("Yosh!")
 
-const usStates = ["What is the capital of Alabama?", "What is the capital of Alaska?", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
-const stateCaps = ["Montgomery", "Juneau", "Phoenix", "Little Rock", "Sacramento", "Denver", "Hartford", " Dover", "Tallahassee", "Atlanta", "Honolulu", "Boise", "Springfield", "Indianapolis", "Des Moines", "Topeka", "Frankfort", "Baton Rouge", "Augusta", "Annapolis", "Boston", "Lansing", "Saint Paul", "Jackson", "Jefferson City", "Helena", "Lincoln", "Carson City", "Concord", "Trenton", "Santa Fe", "Albany", "Raleigh", "Bismarck", "Columbus", "Oklahoma City", "Salem", "Harrisburg", "Providence", "Columbia", "Pierre", "Nashville", "Austin", "Salt Lake City", "Montpelier", "Richmond", "Olympia", "Charleston", "Madison", "Cheyenne"]
+const usStates = ["What is the capital of Alabama?", "What is the capital of Alaska?"] // Temp --- shortened array for testing --- "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+const stateCaps = ["Montgomery", "Juneau"] // Temp -- shortened array for testing --- "Phoenix", "Little Rock", "Sacramento", "Denver", "Hartford", " Dover", "Tallahassee", "Atlanta", "Honolulu", "Boise", "Springfield", "Indianapolis", "Des Moines", "Topeka", "Frankfort", "Baton Rouge", "Augusta", "Annapolis", "Boston", "Lansing", "Saint Paul", "Jackson", "Jefferson City", "Helena", "Lincoln", "Carson City", "Concord", "Trenton", "Santa Fe", "Albany", "Raleigh", "Bismarck", "Columbus", "Oklahoma City", "Salem", "Harrisburg", "Providence", "Columbia", "Pierre", "Nashville", "Austin", "Salt Lake City", "Montpelier", "Richmond", "Olympia", "Charleston", "Madison", "Cheyenne"]
 
 
+// for (let i = 0; i <= usStates.length; i++) { }
 
+/*
+// Attempted to used test() function to enter quiz question into HTML. No luck.
 
-for (let i = 0; i <= usStates.length; i++) {
-    document.querySelector("#city").innerHTML = usStates // This would not do the job even it worked.
-                                                         // It would interate through th entie array without stopping.    
- 
+function test() {   
+    document.querySelector("#city").innerHTML = "Use to enter question from usStates array into HTML p tag id=city"                                                              
+    }
+    test()
+
+*/
+
+/*
+
+// Attempted to used test2() function to enter quiz question into HTML. No luck.
+
+function test2() {
+document.getElementById("city").innerHTML = "Use to enter question from usStates array into HTML p tag id=city"
 }
+test2();
+
+*/
+
+
+
 
 
 
