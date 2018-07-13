c0bf2f248cf8c4d44f5469135b59f0586df18704
Boku <Boku@NHs-MacBook-Pro.local>
2018-07-13 07:24:24 -0400

    Started draft markdown file. Modified h1 and Title in html file. Modified comments in JS file.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Project-1
+
+#State Capital Quiz
+##The purpose of the State Capital Quiz application is to gamify the process of learning the 50 U.S State Capaitals.
--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("Yosh!")
 
 const usStates = ["What is the capital of Alabama?", "What is the capital of Alaska?"] // Temp --- shortened array for testing --- "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
 const stateCap = ["Montgomery", "Juneau"] // Temp -- shortened array for testing --- "Phoenix", "Little Rock", "Sacramento", "Denver", "Hartford", " Dover", "Tallahassee", "Atlanta", "Honolulu", "Boise", "Springfield", "Indianapolis", "Des Moines", "Topeka", "Frankfort", "Baton Rouge", "Augusta", "Annapolis", "Boston", "Lansing", "Saint Paul", "Jackson", "Jefferson City", "Helena", "Lincoln", "Carson City", "Concord", "Trenton", "Santa Fe", "Albany", "Raleigh", "Bismarck", "Columbus", "Oklahoma City", "Salem", "Harrisburg", "Providence", "Columbia", "Pierre", "Nashville", "Austin", "Salt Lake City", "Montpelier", "Richmond", "Olympia", "Charleston", "Madison", "Cheyenne"]
-
+ 
 
 
 let q1 = usStates[0]
@@ -114,6 +114,8 @@ let a50 = stateCap[49]
 
 // for (let i = 0; i <= usStates.length; i++) {
 //        console.log(usStates[i])
+
+
 //}
 
 
