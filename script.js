e9f87b9fe7305474ddb207b1dfce3d7dfe0e53ae
Boku <Boku@NHs-MacBook-Pro.local>
2018-07-13 13:57:39 -0400

    Added minor body styling to css. Modified comments and cleaned up blank lines in JS file.

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-console.log("Yosh!")
-
-const usStates = ["What is the capital of Alabama?", "What is the capital of Alaska?"] // Temp --- shortened array for testing --- "What is the capital of Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
-const stateCap = ["Montgomery", "Juneau"] // Temp -- shortened array for testing --- "Phoenix", "Little Rock", "Sacramento", "Denver", "Hartford", " Dover", "Tallahassee", "Atlanta", "Honolulu", "Boise", "Springfield", "Indianapolis", "Des Moines", "Topeka", "Frankfort", "Baton Rouge", "Augusta", "Annapolis", "Boston", "Lansing", "Saint Paul", "Jackson", "Jefferson City", "Helena", "Lincoln", "Carson City", "Concord", "Trenton", "Santa Fe", "Albany", "Raleigh", "Bismarck", "Columbus", "Oklahoma City", "Salem", "Harrisburg", "Providence", "Columbia", "Pierre", "Nashville", "Austin", "Salt Lake City", "Montpelier", "Richmond", "Olympia", "Charleston", "Madison", "Cheyenne"]
- 
+const usStates = ["What is the capital of Alabama?", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+const stateCap = ["Montgomery", "Juneau", "Phoenix", "Little Rock", "Sacramento", "Denver", "Hartford", " Dover", "Tallahassee", "Atlanta", "Honolulu", "Boise", "Springfield", "Indianapolis", "Des Moines", "Topeka", "Frankfort", "Baton Rouge", "Augusta", "Annapolis", "Boston", "Lansing", "Saint Paul", "Jackson", "Jefferson City", "Helena", "Lincoln", "Carson City", "Concord", "Trenton", "Santa Fe", "Albany", "Raleigh", "Bismarck", "Columbus", "Oklahoma City", "Salem", "Harrisburg", "Providence", "Columbia", "Pierre", "Nashville", "Austin", "Salt Lake City", "Montpelier", "Richmond", "Olympia", "Charleston", "Madison", "Cheyenne"]
 
 
+
+// The  long list of 'q' variables below are unnecessary. Remove them after completing work with the arrays.
 let q1 = usStates[0]
 let q2 = usStates[1]
 let q3 = usStates[2]
@@ -108,53 +107,43 @@ let a49 = stateCap[48]
 let a50 = stateCap[49]
 
 
+// Standard 'for loop. Not sure if I'm going to use it now. Currently just logging to console.
+
+for (let i = 0; i <= usStates.length; i++) {
+    console.log(usStates[i])
+}
 
 
-// standard 'for loop that I'm not using yet
-
-// for (let i = 0; i <= usStates.length; i++) {
-//        console.log(usStates[i])
-
-
-//}
-
-
-// renders quiz questions in the HTML paragraph - still need to automate / interate
-// one-at-a-time through usStates array
-
+// Renders quiz questions in the HTML p tag - need to interate through array
 
  function askQuestion() {  
-    
-    document.querySelector("#city").innerHTML = q1                                     
+   for (let i = 0; i <= usStates.length; i++) {
+        //console.log(usStates[i])
+        document.querySelector("#city").innerHTML = usStates[0]
+    }                                    
 }
     askQuestion()
 
+//Invoked by 'onclick' method on the HTML 'Check Answer' button
 
-
-
-// Trying to get this to work in conjunction with the 'onclick' method on the HTML button
-
-    function checkAnsFunction() {
-        let answer = document.querySelector("#enterAns").value
-        if (answer === a1) {
+function checkAnsFunction() {
+    let answer = document.querySelector("#enterAns").value
+    if (answer === a1) {
             document.querySelector("#response").innerHTML = "You Nailed It! You Rule!"
-            // alert("You nailed it!");
-        }   
-        else {
-            //alert("Sorry, try again")
-       
-          document.querySelector("#response").innerHTML = "Sorry, try again" 
-        }
     }   
+    else {
+          document.querySelector("#response").innerHTML = "Sorry, try again" 
+    }
+ }   
     checkAnsFunction()
+
 
 
 
 /*
 
-
 for (let i = 0; i <= usStates.length; i++) {    
-    if (usStates[i] === stateCap) {
+    if (usStates[i] === stateCap[0]) {
         console.log("yes");
     }
     else {
